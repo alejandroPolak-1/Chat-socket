fb552aa9b761ec8cf0f48aacb095bcfaed416871
alejandroPolak <alejandro_polak@hotmail.com>
2021-01-12 00:13:52 -0300

    connection ok

--- server/server.js
+++ server/server.js
@@ -14,10 +14,37 @@ app.use(express.static(publicPath))
 
 // IO = this is the backend communication
 
-module.exports.io = socketIO(server)
-require('./sockets/socket')
+let io = socketIO(server)
 
-app.listen(port, (err) => {
+io.on('connection', (client) => {
+  console.log('Connected user')
+
+  client.emit('sendMessage', {
+    usuario: 'Admin',
+    mensaje: 'Welcome to this app',
+  })
+
+  client.on('disconnect', () => {
+    console.log('Disconnected user')
+  })
+
+  // Listen to client
+  client.on('sendMessage', (data, callback) => {
+    console.log(data)
+
+    if (data.user) {
+      callback({
+        resp: 'All went well!',
+      })
+    } else {
+      callback({
+        resp: 'Everything went wrong!!!!!!!!',
+      })
+    }
+  })
+})
+
+server.listen(port, (err) => {
   if (err) throw new Error(err)
 
   console.log(`Server run in port ${port}`)
